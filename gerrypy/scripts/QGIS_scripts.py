ed3b1f34ada92e1b59f103c8fc90ef127f71b72c
Julien <julienawilson@gmail.com>
2017-01-08 14:16:29 -0800

    qgis scripts working now

--- gerrypy/scripts/QGIS_scripts.py
+++ gerrypy/scripts/QGIS_scripts.py
@@ -1,6 +1,6 @@
 """PyQGIS scripts for manipulting spatial data."""
 
-# from qgis.core import QgsProject
+from qgis.core import QgsProject
 from PyQt4.QtCore import QFileInfo
 
 project = QgsProject.instance()
