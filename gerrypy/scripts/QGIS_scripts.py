8c096d19a22b67730b9211d7b74c4d66ccbfea7a
Julien <julienawilson@gmail.com>
2017-01-08 15:33:05 -0800

    vector layers in qgis

--- gerrypy/scripts/QGIS_scripts.py
+++ gerrypy/scripts/QGIS_scripts.py
@@ -1,12 +1,26 @@
 """PyQGIS scripts for manipulting spatial data."""
 
-from qgis.core import QgsProject
+from qgis.core import QgsProject, QgsDataSourceURI, QgsVectorLayer
 from PyQt4.QtCore import QFileInfo
 
 project = QgsProject.instance()
 
-print(project.fileName)
+print(project.fileName())
 
 project.write(QFileInfo('../models/GIS_data/gerrypy.qgs'))
 
-print(project.fileName)
+print(project.fileName())
+
+#Add vector data
+
+uri = QgsDataSourceURI()
+
+uri.setConnection("localhost", "5432", "gisdb", "julieanwilson", "postword!!")
+uri.setDataSource("public", "colorado_tracts", "geom")
+
+CO_tracts = QgsVectorLayer(uri.uri(False), "CO_tracts", "postgres")
+
+if not CO_tracts:
+    print("Layer failed to load!")
+else:
+    print("it worked!")
