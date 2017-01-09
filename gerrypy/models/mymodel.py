f6aae08485984167cc3bec2ad5c9ff0aa0c6caf4
jordan <j.schatzman@outlook.com>
2017-01-09 11:12:06 -0800

    adding initial models

--- gerrypy/models/mymodel.py
+++ gerrypy/models/mymodel.py
@@ -4,7 +4,8 @@ from sqlalchemy import (
     Column,
     Index,
     Integer,
-    Float
+    Float,
+    Numeric
 )
 
 from .meta import Base
@@ -18,6 +19,19 @@ class District(Base):
     district_number = Column(Integer)
     area = Column(Float)
     population = Column(Integer)
+
+
+class Tract(Base):
+    """Graph used to assign districts to tracts in the db."""
+    __tablename__ == 'colorado_tracts'
+    gid = Column(Integer, primary_key=True)
+    districtid = Column(Integer)
+    shape_area = Column(Numeric)
+    tract_pop = Column(Integer)
+
+    # def assign_distict(self, id):
+    #     """Assign district to the tract."""
+    #     self.districtid = id
 
 
 Index('district_num', District.district_number, unique=True, mysql_length=255)
