d83b10943948d4b82832b9bff486aa4339ec0a5e
jordan <j.schatzman@outlook.com>
2017-01-10 14:31:06 -0800

    create test state

--- gerrypy/test_db.py
+++ gerrypy/test_db.py
@@ -3,6 +3,7 @@ import transaction
 from pyramid import testing
 from gerrypy.models.mymodel import Tract, District, Edge
 from gerrypy.models.meta import Base
+from gerrypy.scripts.fish_scales import State, OccupiedDist
 from gerrypy.graph_db_interact.assigndistrict import assign_district, populate_district_table
 import sys
 import os
@@ -61,60 +62,82 @@ def filled_graph(dummy_request):
     from gerrypy.scripts.fish_scales import fill_graph
     return fill_graph(dummy_request)
 
+
+@pytest.fixture
+def sample_state():
+    """Create a sample state with some district data."""
+    test_district1 = OccupiedDist()
+    test_district1.population = 500
+    test_district1.area = 2500
+    test_district1.districtID = 1
+    test_district2 = OccupiedDist()
+    test_district2.population = 53043
+    test_district2.area = 250420
+    test_district2.districtID = 2
+    test_district3 = OccupiedDist()
+    test_district3.population = 123
+    test_district3.area = 456
+    test_district3.districtID = 789
+    test_state = State()
+    test_state.districts.extend([test_district1, test_district2, test_district3])
+    return test_state
+
+
+
 # ------DB Tests--------
 
 
-# def test_database_has_tracts(db_session):
-#     """Test that database has contents."""
-#     assert db_session.query(Tract).count() == 1249
+def test_database_has_tracts(db_session):
+    """Test that database has contents."""
+    assert db_session.query(Tract).count() == 1249
 
 
-# def test_database_has_edges(db_session):
-#     """Test that database has contents."""
-#     assert db_session.query(Edge).count() == 15948
+def test_database_has_edges(db_session):
+    """Test that database has contents."""
+    assert db_session.query(Edge).count() == 15948
 
 
-# def test_edit_districtid(db_session):
-#     """Test that editing district works correctly."""
-#     sample_row = db_session.query(Tract).first()
-#     sample_row.disrictid = 50
-#     assert sample_row.disrictid == 50
+def test_edit_districtid(db_session):
+    """Test that editing district works correctly."""
+    sample_row = db_session.query(Tract).first()
+    sample_row.disrictid = 50
+    assert sample_row.disrictid == 50
 
 
-# def test_empty_district_nums(dummy_request, filled_graph):
-#     """Test that all districts have no district id before they're filled."""
-#     query = dummy_request.dbsession.query(Tract)
-#     no_d_id = query.filter(Tract.districtid == None).count()
-#     assert no_d_id == 1249
+def test_empty_district_nums(dummy_request, filled_graph):
+    """Test that all districts have no district id before they're filled."""
+    query = dummy_request.dbsession.query(Tract)
+    no_d_id = query.filter(Tract.districtid == None).count()
+    assert no_d_id == 1249
 
 
-# def test_assign_district_add_one_dist_id(dummy_request, filled_graph):
-#     """Test that a district id is filled by assign_district."""
-#     nx.nodes(filled_graph)[0].districtid = 3
-#     assign_district(dummy_request, filled_graph)
-#     query = dummy_request.dbsession.query(Tract)
-#     no_d_id = query.filter(Tract.districtid == None).count()
-#     assert no_d_id == 1248
+def test_assign_district_add_one_dist_id(dummy_request, filled_graph):
+    """Test that a district id is filled by assign_district."""
+    nx.nodes(filled_graph)[0].districtid = 3
+    assign_district(dummy_request, filled_graph)
+    query = dummy_request.dbsession.query(Tract)
+    no_d_id = query.filter(Tract.districtid == None).count()
+    assert no_d_id == 1248
 
 
-# def test_assign_district_adds_mult_dist_ids(dummy_request, filled_graph):
-#     """Test that multiple district ids are filled by assign_district."""
-#     nx.nodes(filled_graph)[0].districtid = 3
-#     nx.nodes(filled_graph)[1].districtid = 4
-#     assign_district(dummy_request, filled_graph)
-#     query = dummy_request.dbsession.query(Tract)
-#     no_d_id = query.filter(Tract.districtid == None).count()
-#     assert no_d_id == 1247
+def test_assign_district_adds_mult_dist_ids(dummy_request, filled_graph):
+    """Test that multiple district ids are filled by assign_district."""
+    nx.nodes(filled_graph)[0].districtid = 3
+    nx.nodes(filled_graph)[1].districtid = 4
+    assign_district(dummy_request, filled_graph)
+    query = dummy_request.dbsession.query(Tract)
+    no_d_id = query.filter(Tract.districtid == None).count()
+    assert no_d_id == 1247
 
 
-# def test_assign_district_correct_dist_assigned(dummy_request, filled_graph):
-#     """Test that assign_district adds the correct district to the db."""
-#     tractid = nx.nodes(filled_graph)[0].gid
-#     nx.nodes(filled_graph)[0].districtid = 3
-#     assign_district(dummy_request, filled_graph)
-#     query = dummy_request.dbsession.query(Tract)
-#     test_tract = query.filter(Tract.gid == tractid).first()
-#     assert test_tract.districtid == 3
+def test_assign_district_correct_dist_assigned(dummy_request, filled_graph):
+    """Test that assign_district adds the correct district to the db."""
+    tractid = nx.nodes(filled_graph)[0].gid
+    nx.nodes(filled_graph)[0].districtid = 3
+    assign_district(dummy_request, filled_graph)
+    query = dummy_request.dbsession.query(Tract)
+    test_tract = query.filter(Tract.gid == tractid).first()
+    assert test_tract.districtid == 3
 
 
 def test_insert_district_table(dummy_request):
@@ -142,5 +165,3 @@ def test_truncate_district_table(dummy_request):
     dummy_request.dbsession.add(test_row2)
     dummy_request.dbsession.query(District).delete()
     assert dummy_request.dbsession.query(District).count() == 0
-
-test_state = State()
