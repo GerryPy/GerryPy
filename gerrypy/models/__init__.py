648a4d784ee4edb1dbcd2f8b151a03cc78e8fb90
jordan <j.schatzman@outlook.com>
2017-01-09 16:15:36 -0800

    more testing for database setup

--- gerrypy/models/__init__.py
+++ gerrypy/models/__init__.py
@@ -5,7 +5,7 @@ import zope.sqlalchemy
 
 # import or define all models here to ensure they are attached to the
 # Base.metadata prior to any initialization routines
-from .mymodel import District  # noqa
+from .mymodel import District, Tract, Edge  # noqa
 
 # run configure_mappers after defining all of the models to ensure
 # all relationships can be setup
