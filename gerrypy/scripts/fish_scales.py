9754bab055ce5165143cd4761bff9d998185e9fb
Avery Pratt <apratt91@gmail.com>
2017-01-11 16:03:54 -0800

    added populate_district_table method call on fill_state

--- gerrypy/scripts/fish_scales.py
+++ gerrypy/scripts/fish_scales.py
@@ -192,8 +192,9 @@ class State(object):
             tgt_population = rem_pop / rem_dist
             self.build_district(tgt_population, num + 1, self.graph)
 
-        from graph_db_interact.assigndistrict import assign_district
+        from graph_db_interact.assigndistrict import assign_district, populate_district_table
         assign_district(self.request, self.graph)
+        populate_district_table(self.request, self)
         if self.unoccupied:
             return False
         return True
