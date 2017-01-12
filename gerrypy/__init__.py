7ff7fdc0d930518b0692245625ed2449786e389b
Julien <julienawilson@gmail.com>
2017-01-11 21:01:57 -0800

    update database env variable for heroku

--- gerrypy/__init__.py
+++ gerrypy/__init__.py
@@ -5,7 +5,7 @@ from pyramid.config import Configurator
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    settings = {'sqlalchemy.url': os.environ["SQL_URL"]}
+    settings = {'sqlalchemy.url': os.environ["DATABASE_URL"]}
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.include('.models')
