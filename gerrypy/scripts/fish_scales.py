635ddc1dc11c2a840ee859adc1aa02dd2e0e44c9
Julien <julienawilson@gmail.com>
2017-02-24 09:41:27 -0800

    fishscales comments and small changes

--- gerrypy/scripts/fish_scales.py
+++ gerrypy/scripts/fish_scales.py
@@ -51,17 +51,16 @@ class OccupiedDist(object):
         """Add node to nodes and updates district properties."""
         node.districtid = self.districtID
         self.nodes.add_node(node)
-        for edge in state_graph.neighbors(node):
-            if edge in self.nodes.nodes():
-                self.nodes.add_edge(edge, node)
-        self.population += node.tract_pop
-        self.area += node.shape_area
+        neighbors = state_graph.neighbors(node)
         if node in self.perimeter:
             self.perimeter.remove(node)
-        neighbors = state_graph.neighbors(node)
-        for neighbor in neighbors:
+        for neighbor in neighbors:  # After node is added, make the edge connections within the occupied district.
+            if neighbor in self.nodes.nodes():
+                self.nodes.add_edge(neighbor, node)
             if neighbor not in self.nodes.nodes() and neighbor not in self.perimeter:
                 self.perimeter.append(neighbor)
+        self.population += node.tract_pop
+        self.area += node.shape_area
 
     def rem_node(self, node, state_graph):
         """Remove node from nodes and updates district properties."""
@@ -70,19 +69,19 @@ class OccupiedDist(object):
         self.area -= node.shape_area
         neighbors = state_graph.neighbors(node)
         to_perimeter = False
-        for neighbor in neighbors:
+        for neighbor in neighbors:  # Decide whether to remove nodes from the district perimeter.
             takeout = True
-            if neighbor in self.perimeter:
+            if neighbor in self.perimeter:  # if its a perimeter node,
                 neighborneighbors = state_graph.neighbors(neighbor)
-                for neighborneighbor in neighborneighbors:
-                    if neighborneighbor in self.nodes.nodes():
-                        takeout = False
-                if takeout:
-                    self.perimeter.remove(neighbor)
-            elif neighbor in self.nodes.nodes():
-                to_perimeter = True
-        if to_perimeter:
-            self.perimeter.append(node)
+                for neighborneighbor in neighborneighbors:  # check its neighbors
+                    if neighborneighbor in self.nodes.nodes():  # if it has a neighbor in the district
+                        takeout = False  # it should remain in the perimeter list.
+                if takeout:  # If it should be removed,
+                    self.perimeter.remove(neighbor)  # Remove it!
+            elif neighbor in self.nodes.nodes():  # If the removed node neighbors the district (which it should)
+                to_perimeter = True  # mark it to be added to the perimeter
+        if to_perimeter:  # If its marked,
+            self.perimeter.append(node)  # add it to the perimeter
 
 
 class UnoccupiedDist(OccupiedDist):
@@ -120,7 +119,7 @@ class UnoccupiedDist(OccupiedDist):
         self.area += node.shape_area
         neighbors = state_graph.neighbors(node)
         to_add = False
-        for neighbor in neighbors:
+        for neighbor in neighbors:  # Handling which nodes to add or remove from the perimeter. 
             takeout = True
             if neighbor in self.perimeter:
                 neighborneighbors = state_graph.neighbors(neighbor)
@@ -140,7 +139,7 @@ class UnoccupiedDist(OccupiedDist):
         self.area -= node.shape_area
         if node in self.perimeter:
             self.perimeter.remove(node)
-        neighbors = self.nodes.neighbors(node) #state_graph.neighbors(node)
+        neighbors = self.nodes.neighbors(node) 
         for neighbor in neighbors:
             if neighbor not in self.perimeter:
                 self.perimeter.append(neighbor)
@@ -162,19 +161,18 @@ class State(object):
         self.districts = []
         self.population = 0
         self.area = 0
-        self.num_dst = num_dst
+        self.num_dst = num_dst # The Number of districts alotted for that state (7 for Colorado)
         self.request = request
         self.state_graph = fill_graph(self.request)
-        landmass = nx.connected_components(self.state_graph)
+        landmass = nx.connected_components(self.state_graph)  # Returns all of the connected/contiguous areas of land for a state.
         for island in landmass:
-            unoc = UnoccupiedDist(None, self.state_graph, tracts=island)
+            unoc = UnoccupiedDist(None, self.state_graph, tracts=island)  # needs the state graph for its edges
             for tract in unoc.nodes.nodes():
-                if tract.isborder == 1:
-                    unoc.perimeter.append(tract)
+                if tract.isborder == 1:  # This is a hardcoded field for Colorado.  A challenge of adding more states is finding these automatically.
+                    unoc.perimeter.append(tract)  # begin with all border tracts in the perimeter.
             self.population += unoc.population
             self.unoccupied.append(unoc)
             self.area += unoc.area
-        self.target_pop = self.population // num_dst
 
     def fill_state(self, criteria):
         """Build districts until all unoccupied tracts are claimed."""
@@ -183,12 +181,9 @@ class State(object):
             for unoc in self.unoccupied:
                 rem_pop += unoc.population
             rem_dist = self.num_dst - len(self.districts)
-            tgt_population = rem_pop / rem_dist
+            tgt_population = rem_pop / rem_dist  # Average available population is the target population.  It helps ensure the State gets totally filled.
             self.build_district(tgt_population, num + 1, criteria)
         assign_district(self.request, self.state_graph)
-        if self.unoccupied:
-            return False
-        return True
 
     def build_district(self, tgt_population, dist_num, criteria):
         """Create a new district stemming from the start node with a given population."""
@@ -198,31 +193,31 @@ class State(object):
         self.swap(dst, start)  # if state is full, this wont work
         while True:
             new_tract = self.select_next(dst, criteria)
-            if new_tract is None:
-                for unoc in self.unoccupied:
+            if new_tract is None:  # If there are no more nodes in unoccupied, this will be None
+                for unoc in self.unoccupied:  # This ends the building process
                     if not len(unoc.nodes.nodes()):
                         self.unoccupied.remove(unoc)
                 break
-            high_pop = (new_tract.tract_pop + dst.population)
-            if abs(high_pop - tgt_population) > abs(dst.population - tgt_population):
-                break
+            high_pop = (new_tract.tract_pop + dst.population)  # Population including the next tract.
+            if abs(high_pop - tgt_population) > abs(dst.population - tgt_population):    # If the population including the next district is further from the goal,
+                break  # We stop building that district
             else:
-                self.swap(dst, new_tract)
-                neighbors = self.state_graph.neighbors(new_tract)
-                unassigned_neighbors = [neighbor for neighbor in neighbors if neighbor in self.unoccupied[0].nodes]
-                if len(unassigned_neighbors) > 1:
-                    for i in range(len(unassigned_neighbors)):
-                        if not nx.has_path(
+                self.swap(dst, new_tract)  # Swap removes the tract from its unoccupied district and adds it to the occupied district.
+                neighbors = self.state_graph.neighbors(new_tract)    
+                unassigned_neighbors = [neighbor for neighbor in neighbors if neighbor in self.unoccupied[0].nodes]  # Grab the new nodes unassigned neighbors
+                if len(unassigned_neighbors) > 1:  # If there is more than one, than a split is possible.
+                    for i in range(len(unassigned_neighbors)):  
+                        if not nx.has_path(    # We check each node and its previous neighbor to ensure they're connected. (thanks, nx)
                             self.unoccupied[0].nodes,
                             unassigned_neighbors[i],
                             unassigned_neighbors[i - 1]
-                        ):
-                            unoc_neighbors = [x for x in nx.connected_components(self.unoccupied[0].nodes)]
+                        ):  # If there is a split in the unoccupied district...
+                            unoc_neighbors = [x for x in nx.connected_components(self.unoccupied[0].nodes)]  # Identify each of the distinct unoccupied districts.
                             biggest = max(unoc_neighbors, key=lambda x: len(x))
-                            unoc_neighbors.remove(biggest)
-
-                            for neigh in unoc_neighbors:
-                                for tract in neigh:
+                            unoc_neighbors.remove(biggest)    # Ignore the largest (This should be highest pop, fixed in different version)
+                            # All unoccupied districts will be bordering, because as soon as there is a split, we do this.
+                            for neigh in unoc_neighbors:  # Consume all of the rest (usually one small one)
+                                for tract in neigh:  # This sometimes gives us a district that is too large, and is the major focus of improving the algorithm.
                                     self.swap(dst, tract)
                             break
 
@@ -234,20 +229,20 @@ class State(object):
     def select_next(self, dst, criteria):
         """Choose the next best tract to add to growing district."""
         best_rating = 0
-        best = None
-        for perimeter_tract in dst.perimeter:
-            if perimeter_tract.districtid is None:
-                count = 0
+        best = None  # We're building a score for each node based on our criteria, and saving the best.
+        for perimeter_tract in dst.perimeter:  # dst.perimeter is every node bordering that district.
+            if perimeter_tract.districtid is None:  # Grab those without a district assigned
+                count = 0    # Check how many tracts that tract borders that are ALREADY in the district.  More borders gets more points
                 for neighbor in self.state_graph.neighbors(perimeter_tract):
                     if neighbor.districtid == dst.districtID:
                         count += 1
-                counties = set()
+                counties = set()    # If the tracts county is in the district already, it gets a point.
                 for node in dst.nodes:
                     counties.add(node.county)
                 same_county = 0
                 if perimeter_tract.county in counties:
-                    same_county = 1
-                rating = count * int(criteria['compactness']) + same_county * int(criteria['county'])
+                    same_county = 1  
+                rating = count * int(criteria['compactness']) + same_county * int(criteria['county'])  # Calculate score based on criteria.
                 if rating > best_rating:
                     best_rating = rating
                     best = perimeter_tract
@@ -259,7 +254,7 @@ class State(object):
         Based on number of bordering districts.
         """
         best_set = set()
-        best = None
+        best = None  
         for tract in self.unoccupied[0].perimeter:
             unique_dists = set()
             for neighbor in self.state_graph.neighbors(tract):
